479c9e6b41f9995355d3d4295adf2229beab3a4b
pbyri <pavansemail@gmail.com>
2017-02-19 19:19:11 -0800

    initial checkin

---
+++ README.txt
@@ -0,0 +1,2 @@
+This is my first checkin to practice using github
+
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, world!" << std::endl;
+    std::vector<uint16_t> vec;
+    for(uint16_t val = 0; val < 10; ++val)
+    {
+      vec.push_back(val);
+    }
+    for(auto val : vec)
+    {
+      std::cout<< val << "\n";
+    }
+    return 0;
+}
